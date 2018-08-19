53369632ada8ced9b31cab8e080bff9f07f02011
Alewdy <alewdys@gmail.com>
2018-08-19 00:31:56 -0400

    draft

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheese burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie burger', false);
